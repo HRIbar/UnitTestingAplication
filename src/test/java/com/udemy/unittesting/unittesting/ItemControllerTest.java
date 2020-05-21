0af3a2392867d7587be3cdd20ce8e3ccedcd5a45
Klemen Hribar <klemen.hribar@hycu.com>
2020-05-21 16:38:04 +0200

    ItemBusinessService, ItemController

--- src/test/java/com/udemy/unittesting/unittesting/ItemControllerTest.java
+++ src/test/java/com/udemy/unittesting/unittesting/ItemControllerTest.java
@@ -1,10 +1,13 @@
 package com.udemy.unittesting.unittesting;
 
 import com.udemy.unittesting.unittesting.business.HelloWorldController;
+import com.udemy.unittesting.unittesting.business.ItemBusinessService;
+import com.udemy.unittesting.unittesting.model.Item;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -13,6 +16,7 @@ import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -23,12 +27,22 @@ public class ItemControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private ItemBusinessService businessService;
+
     @Test
-    public void helloWorld_basic() throws Exception {
-        RequestBuilder request = MockMvcRequestBuilders.get("/hello-world").accept(MediaType.APPLICATION_JSON);
+    public void itemFromBusinessService_basic() throws Exception {
+
+        when(businessService.retrieveHardcodedItem()).thenReturn(new Item(2,"Item2",10,10));
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/item-from-business-service")
+                .accept(MediaType.APPLICATION_JSON);
+
+
         MvcResult result = mockMvc.perform(request)
                 .andExpect(status().isOk())
-                .andExpect(content().string("Hello World"))
+                .andExpect(content().json("{id:1,name:Ball,price:10,quantity:100}"))
                 .andReturn();
 
         //verfy Hello World
